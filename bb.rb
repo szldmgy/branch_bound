bb39675bb9bce683ef89d0adea3bb0b2fe63d10a
Mateusz Lenik <mt.lenik@gmail.com>
2011-11-05 19:12:55 +0100

    Moved core extensions to separate file

--- bb.rb
+++ bb.rb
@@ -1,37 +1,7 @@
 #!/usr/bin/env ruby
 
-class NilClass
-  def try(method, *args, &block)
-    nil
-  end
-end
-
-class BasicObject
-  def try(method, *args, &block)
-    send method, *args, &block
-  end
-end
-
-class Array
-  def permutate(&block)
-    array = Array.new size, nil
-    permutate_re array, 0, block
-  end
-
-  private
-  def permutate_re(array, level = 0, block) # :nodoc:
-    unless level == array.size
-      array = array.dup
-      each do |task|
-        next if array.include? task
-        array[level] = task
-        unless block[array]
-          permutate_re array, level + 1, block
-        end
-      end
-    end
-  end
-end
+$:.unshift File.dirname(__FILE__)
+require 'extensions'
 
 module BB
   class BB
